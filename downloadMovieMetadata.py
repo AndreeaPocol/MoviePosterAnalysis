e4a96bd520155bb4eae7117ac163d6180f697f1b
Andreea Pocol <apocol@uwaterloo.ca>
2023-07-12 10:51:12 -0400

    Handle connection dropping

--- downloadMovieMetadata.py
+++ downloadMovieMetadata.py
@@ -1,42 +1,37 @@
 from tqdm import tqdm
 import requests
 import json
-import pandas as pd
-import numpy as np
 
-basics = pd.read_csv("title.basics.tsv", delimiter="\t")
-basics["startYear"] = basics["startYear"].astype(str).replace("\\N", np.NaN).astype(float)
-filter_data = basics[(basics["titleType"] == "movie") & (basics["startYear"] >= 1960)]
+key = "1844b7ba"
 
-print(filter_data[filter_data["tconst"] == "tt0015414"]) # example
-
-with open("tconsts_small.txt", "w") as f: # example
-    f.write("\n".join(filter_data["tconst"][1:10].tolist()))
-
-with open("tconsts.txt", "w") as f:
-    f.write("\n".join(filter_data["tconst"].tolist()))
-
-with open("tconsts.txt") as f:
+with open("tconsts_small.txt") as f:
     tconsts = f.read().splitlines()
 
 data = dict()
 problematic = []
+connectionDropped = []
 
 for tid in tqdm(tconsts):
     try:
+        print(f"Processing {tid}...")
         data[tid] = requests.get(
-            "http://www.omdbapi.com/", params={"apikey": "48d60ec2", "i": tid}
+            "http://www.omdbapi.com/", 
+            params={"apikey": key, "i": tid}, 
+            timeout=20
         ).json()
     except json.JSONDecodeError:
         try:
             data[tid] = json.loads(
                 requests.get(
                     "http://www.omdbapi.com/",
-                    params={"apikey": "48d60ec2", "i": tid},
+                    params={"apikey": key, "i": tid},
                 ).text.replace("\\", "\\\\")
             )
         except Exception:
             problematic.append(tid)
+    except requests.Timeout:
+        print(f'Connection dropped while processing {tid}')
+        problematic.append(tid)
     except Exception:
         problematic.append(tid)
 
@@ -44,6 +39,8 @@ for tid in tqdm(tconsts):
 with open("omdb_movies.json", "w") as f:
     json.dump(data, f)
 
-
 with open("problematic_tconsts.txt", "w") as f:
     f.write("\n".join(problematic))
+    
+with open("connection_dropped_tconsts.txt", "w") as f:
+    f.write("\n".join(connectionDropped))
