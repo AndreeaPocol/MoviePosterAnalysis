b67dfee6c729f714949c71b0bcc99f0b8bd723a9
Andreea Pocol <apocol@uwaterloo.ca>
2023-07-20 11:20:39 -0400

    parallelizing movie poster analysis

--- analyzeMoviePoster.py
+++ analyzeMoviePoster.py
@@ -8,13 +8,19 @@ import requests
 from PIL import Image
 from io import BytesIO
 import json
+import os
+from multiprocessing import Pool
 
 
-ages = defaultdict(int)
-races = defaultdict(int)
-genders = defaultdict(int)
+AVAILABLE_CPUS = os.cpu_count() - 1
+if AVAILABLE_CPUS == 0:
+    AVAILABLE_CPUS = 1
 
-posters_without_faces = []
+AGES = {}
+RACES = {}
+GENDERS = {}
+NOFACES = []
+
 backends = [
   'opencv', 
   'ssd', 
@@ -24,7 +30,7 @@ backends = [
   'mediapipe'
 ]
 
-def writeOutput(my_dict, filename):
+def writeDictToCsv(my_dict, filename):
     with open(filename, 'w') as f:
         for key in my_dict.keys():
             f.write("%s,%s\n"%(key,my_dict[key]))
@@ -43,25 +49,45 @@ def detectFaces(img):
         # do something
 
 
-def analyzePoster(img):
-    analysis = DeepFace.analyze(
-        img_path = img, 
-        enforce_detection = True,
-        actions = ["age", "gender", "race"],
-        detector_backend = backends[3]
-    )
-    for face_analysis in analysis:
-        age = face_analysis['age']
-        gender = face_analysis['dominant_gender']
-        race = face_analysis['dominant_race']
-        print(f"Detected a {age}-year-old {race.lower()} {gender.lower()}...")
-        ages[age] += 1
-        genders[gender] += 1
-        races[race] += 1
+def analyzePoster(movies):
+    ages = defaultdict(int)
+    races = defaultdict(int)
+    genders = defaultdict(int)
+    posters_without_faces = []
+
+    for movie in movies:
+        try:
+            url = movie[1]['Poster']
+            tconst = movie[0]
+            poster = requests.get(url)
+            img = Image.open(BytesIO(poster.content))
+            analysis = DeepFace.analyze(
+                img_path = img, 
+                enforce_detection = True,
+                actions = ["age", "gender", "race"],
+                detector_backend = backends[3]
+            )
+            for face_analysis in analysis:
+                age = face_analysis['age']
+                gender = face_analysis['dominant_gender']
+                race = face_analysis['dominant_race']
+                print(f"Detected a {age}-year-old {race.lower()} {gender.lower()}...")
+                ages[age] += 1
+                genders[gender] += 1
+                races[race] += 1
+        except Exception:
+            print(f"Unable to detect face for {tconst}...")
+            posters_without_faces.append(tconst)
+    return ages, races, genders, posters_without_faces
 
 
 def main():
+    global AGES
+    global RACES
+    global GENDERS
+    global NOFACES
     movieFile = ""
+
     if len(sys.argv) == 2:
         movieFile = sys.argv[1]
     else:
@@ -75,22 +101,30 @@ def main():
     with open(movieFile) as f:
         movies = json.load(f)
 
-    for movie in movies:
-        url = movie['Poster']
-        name = movie['Title']
-        poster = requests.get(url)
-        img = Image.open(BytesIO(poster.content))
-        try:
-            analyzePoster(img)
-        except Exception:
-            print(f"Unable to detect face for {name}")
-            posters_without_faces.append(name)
+    # chunk movies into AVAILABLE_CPUS of work
+    startAt = 0
+    endAt = 3 #232430
+    numToProcess = endAt - startAt
+    numMoviesPerChunk = (int)(numToProcess / AVAILABLE_CPUS) + 1
+    print(f"Number of movies per chunk: {numMoviesPerChunk}")
+    moviesChunked = [list(movies.items())[i:i + numMoviesPerChunk] for i in range(startAt, endAt, numMoviesPerChunk)]
+    pool = Pool(processes=AVAILABLE_CPUS)
+    results = pool.map(analyzePoster, moviesChunked)
+    pool.close()
+    pool.join()
 
-    writeOutput(ages, "ages.csv")
-    writeOutput(races, "races.csv")
-    writeOutput(genders, "genders.csv")
-    print("Could not detect faces for: ", posters_without_faces)
+    # JOIN all dictionaries into ONE
+    for result in results:
+        AGES |= result[0]
+        RACES |= result[1]
+        GENDERS |= result[2]
+        NOFACES += result[3]
 
+    writeDictToCsv(AGES, f"ages{endAt}.csv")
+    writeDictToCsv(RACES, f"races{endAt}.csv")
+    writeDictToCsv(GENDERS, f"genders{endAt}.csv")
+    with open(f"noFaces{endAt}.txt",'w') as tfile:
+        tfile.write('\n'.join(NOFACES))
 
 if __name__ == "__main__":
     main()
