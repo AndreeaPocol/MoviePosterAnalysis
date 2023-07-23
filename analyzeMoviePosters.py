dfbc308aba1a2f1ebc98eb4642543ce2a88b7bce
Andreea Pocol <apocol@uwaterloo.ca>
2023-07-23 15:59:17 -0400

    generate detailed diversity dataset (not just global stats)

--- analyzeMoviePosters.py
+++ analyzeMoviePosters.py
@@ -6,7 +6,8 @@ from mtcnn import MTCNN
 import glob
 import os
 from multiprocessing import Pool
-from collections import Counter
+import json
+import csv
 
 AVAILABLE_CPUS = os.cpu_count() - 1
 if AVAILABLE_CPUS == 0:
@@ -21,10 +22,22 @@ backends = [
   'mediapipe'
 ]
 
-def writeDictToCsv(my_dict, filename):
-    with open(filename, 'w') as f:
-        for key in my_dict.keys():
-            f.write("%s,%s\n"%(key,my_dict[key]))
+
+posterDir = ""
+movieDataFile = ""
+if len(sys.argv) == 3:
+    posterDir = sys.argv[1]
+    movieDataFile = sys.argv[2]
+else:
+    print(
+        "Usage: {name} [ analyzeMoviePoster metadataFile]".format(
+            name=sys.argv[0]
+        )
+    )
+    exit()
+paths = glob.glob(f"{posterDir}/*")
+with open(movieDataFile) as json_file:
+    movieData = json.load(json_file)
 
 
 def detectFaces(img):
@@ -40,13 +53,27 @@ def detectFaces(img):
         # do something
 
 
+def ageBracket(age):
+    if age <= 12:
+        return "children"
+    elif age <= 17:
+        return "adolescents"
+    elif age <= 65:
+        return "adults"
+    else:
+        return "older-adults"
+
+
 def analyzePoster(movies):
-    ages = Counter({})
-    races = Counter({})
-    genders = Counter({})
     posters_without_faces = []
+    csvRows = []
 
     for movie in movies:
+        # deepface classifications
+        # https://github.com/serengil/deepface/tree/1321435a303f21e39cb7f0fff67582449768846f/deepface/extendedmodels
+        agesInThisPoster = {"children": 0, "adolescents": 0, "adults": 0, "older-adults": 0}
+        racesInThisPoster = {"asian": 0, "indian": 0, "black": 0, "white": 0, "middle eastern": 0, "latino hispanic": 0}
+        gendersInThisPoster = {"Woman": 0, "Man": 0}
         try:
             analysis = DeepFace.analyze(
                 img_path = movie, 
@@ -54,38 +81,48 @@ def analyzePoster(movies):
                 actions = ["age", "gender", "race"],
                 detector_backend = backends[3]
             )
+
             for face_analysis in analysis:
                 age = face_analysis['age']
                 gender = face_analysis['dominant_gender']
                 race = face_analysis['dominant_race']
                 print(f"Detected a {age}-year-old {race.lower()} {gender.lower()}...")
-                ages[age] += 1
-                genders[gender] += 1
-                races[race] += 1
+                agesInThisPoster[ageBracket(age)] += 1
+                gendersInThisPoster[gender] += 1
+                racesInThisPoster[race] += 1
+            
+            tconst = os.path.splitext(os.path.basename(movie))[0]
+            print("tconst: ", tconst)
+            metadata = movieData[tconst]
+            csvRow = [tconst,
+                      metadata['Title'],
+                      metadata['Year'],
+                      metadata['Genre'],
+                      metadata['Country'],
+                      agesInThisPoster["children"],
+                      agesInThisPoster["adolescents"],
+                      agesInThisPoster["adults"],
+                      agesInThisPoster["older-adults"],
+                      racesInThisPoster["asian"],
+                      racesInThisPoster["indian"],
+                      racesInThisPoster["black"],
+                      racesInThisPoster["white"],
+                      racesInThisPoster["middle eastern"],
+                      racesInThisPoster["latino hispanic"],
+                      gendersInThisPoster["Man"],
+                      gendersInThisPoster["Woman"]
+                    ]
+            csvRows.append(csvRow)
+
         except Exception:
             print(f"Unable to detect face for {movie}...")
             posters_without_faces.append(movie)
-    return ages, races, genders, posters_without_faces
+    return posters_without_faces, csvRows
 
 
 def main():
-    AGES = Counter({})
-    RACES = Counter({})
-    GENDERS = Counter({})
     NOFACES = []
-
-    dir_path = ""
-    if len(sys.argv) == 2:
-        dir_path = sys.argv[1]
-    else:
-        print(
-            "Usage: {name} [ analyzeMoviePoster ]".format(
-                name=sys.argv[0]
-            )
-        )
-        exit()
-    
-    paths = glob.glob(f"{dir_path}/*")
+    ROWS = []
 
     # chunk movies into AVAILABLE_CPUS of work
     startAt = 0
@@ -101,16 +138,35 @@ def main():
 
     # JOIN all dictionaries into ONE
     for result in results:
-        AGES += result[0]
-        RACES += result[1]
-        GENDERS += result[2]
         NOFACES += result[3]
+        ROWS += result[4]
 
-    writeDictToCsv(AGES, f"ages.csv")
-    writeDictToCsv(RACES, f"races.csv")
-    writeDictToCsv(GENDERS, f"genders.csv")
-    with open(f"no_faces.txt",'w') as tfile:
+    with open("no_faces.txt",'w') as tfile:
         tfile.write('\n'.join(NOFACES))
+
+    header = ['id',
+              'title',
+              'year',
+              'genre',
+              'country',
+              'children',
+              'adolescents',
+              'adults',
+              'older-adults',
+              'asians',
+              'indians',
+              'blacks',
+              'whites',
+              'middle-easterns',
+              'latino-hispanics',
+              'men',
+              'women'
+            ]
+    with open("diversity-dataset.csv", 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerows(ROWS)
+
 
 if __name__ == "__main__":
     main()
